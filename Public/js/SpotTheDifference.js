2c163ec74d1f5fd7700175d2ebc81f6c3ab19b25
Jonathan Miller <jonathanbmi10@gmail.com>
2014-11-27 17:18:01 -0500

    Intro Page; Level 1 Intro; Level 1 Spot; Level 1 Quiz; Completed.

--- Public/js/SpotTheDifference.js
+++ Public/js/SpotTheDifference.js
@@ -31,29 +31,60 @@
 		
 		
 		setInterval(function () {
-		    that.time = Math.trunc((Date.now() - that.startTime)/ 1000);
-		    $("#time").text(that.time);
+		    that.time = Math.trunc((Date.now() - that.startTime) / 1000);
+		    if (that.time >= 99) that.time = 99;
+		    var ones = 0;
+		    var tens = 0;
+		    if (that.time >= 10) {
+		        ones = String(that.time).charAt(1);
+		        tens = String(that.time).charAt(0);
+		    }
+		    else {
+		        ones = String(that.time).charAt(0);
+		        tens = 0;
+		    }
+
+		    $("#clock-ones").attr("src", "/Public/img/clock-" + ones + ".jpg");
+		    $("#clock-tens").attr("src", "/Public/img/clock-" + tens + ".jpg");
 		}, 100);
 		var gameCarousel = $("#carousel-spot-the-difference");
 		$("#carousel-spot-the-difference").carousel('pause');
 		
-		 
-	    // Tile1-> GoButton Handler
+		$("body").keydown(function (e) {
+		    if (e.keyCode == 37) { // left
+		        $(gameCarousel).carousel('prev');
+		    }
+		    else if (e.keyCode == 39) { // right
+		        $(gameCarousel).carousel('next');
+		    }
+		});
+
+	    // Intro Screen
 		$("#GoButton").click(function () {
 		    if ($('#username').val() != "")
 		    {
 		        that.username = $("#username").val();
 		        $("#usernameLabel").text(that.username);
 		        $(gameCarousel).carousel('next');
-		        $(gameCarousel).carousel('pause');
-		        that.startTime = Date.now();
-		        $("#points").text(that.score);
-		        $("#ui-info").show();
-		        $(this).hide();
-		    }
+            }
+		});
+		$("#HowToPlayButton").click(function () {
+		    $("#Modal-HowToPlay").modal("show");
 		});
 
-	    // Tile2-> Spot Handler
+	    // Level 1 Intro Screen
+		$("#Level1IntroScreen").click(function () {
+		    $(gameCarousel).carousel('next');
+		    $("#Modal-StartSpotLevel1").modal("show");
+        });
+
+	    // Level 1 Spot
+		$("#Start-SpotLevel1").click(function () {
+		    that.startTime = Date.now();
+		    $("#points").text(that.score);
+		    $("#ui-info").show();
+		    $("#Modal-StartSpotLevel1").modal("hide");
+		});
 		$(".spot").click(function () {
 		    var id = $(this).attr("data-id");
 		    if (!$(this).hasClass("found"))
@@ -67,26 +98,34 @@
 		    }
 		    if(that.numbFound == 3)
 		    {
-		        $(gameCarousel).carousel('next');
-		        $(gameCarousel).carousel('pause');
-		        that.startTime = Date.now();
+		        $("#Modal-EndSpotLevel1").modal("show");
             }
 		});
+		$("#End-SpotLevel1").click(function () {
+		    $("#Modal-EndSpotLevel1").modal("hide");
+		    $(gameCarousel).carousel('next');
+		    that.startTime = Date.now();
+		});
 
-	    // Tile3-> Quiz Handler
+	    // Level 1 Quiz
 		$("input[name='popquiz1']").click(function () {
 		    var correct = $(this).attr("data-correct");
 		    if(correct == "true")
 		    {
 		        that.AddPoints();
-		        $(gameCarousel).carousel('next');
-		        $(gameCarousel).carousel('pause');
-		    }
+		        $("#Modal-EndQuizLevel1").modal("show");
+            }
 		    else
 		    {
 		        $(this).prop('checked', false);
 		        that.MinusPoints();
+		        $("#Modal-IncorrectQuizLevel1").modal("show");
             }
+		});
+		$("#End-QuizLevel1").click(function () {
+		    $("#Modal-EndQuizLevel1").modal("hide");
+		    $(gameCarousel).carousel('next');
+		    that.startTime = Date.now();
 		});
 
 		
