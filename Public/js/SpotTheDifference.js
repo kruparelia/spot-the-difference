176aece0e865e9bea95821fe756f3e753d41386d
Jonathan Miller <jonathanbmi10@gmail.com>
2014-11-28 12:21:10 -0500

    Level 2, up to Level 3's Spot Round, Completed.

--- Public/js/SpotTheDifference.js
+++ Public/js/SpotTheDifference.js
@@ -11,10 +11,12 @@
 		this.score = 0;
 		this.username = "";
 		this.numbFound = 0;
+		this.toBeFound = 10;
 		this.startTime = Date.now();
 		this.time = this.startTime;
-	  
-	    // INIT 
+		this.finishFunction = function () { };
+		this.incorrectFunction = function () { };
+        // INIT 
 		this.initalize();
   }
   
@@ -59,6 +61,40 @@
 		    }
 		});
 
+		$(".spot").click(function () {
+		    var id = $(this).attr("data-id");
+		    if (!$(this).hasClass("found")) {
+		        that.numbFound++;
+		        $(".spot[data-id='" + id + "']").each(function () {
+		            $(this).addClass("found");
+		            $(this).css("border", "5px solid white");
+		        });
+		        that.AddPoints();
+		    }
+		    if (that.numbFound == that.toBeFound) {
+		        that.finishFunction();
+		    }
+		});
+		$(".spot-level").click(function (e) {
+		    var x = e.pageX - 24;
+		    var y = e.pageY - 24;
+		    $("body").append("<img class='xmark' src='/Public/img/x-mark.png' style='position:absolute; top:" + y + "px; left:" + x + "px;' />");
+		    setTimeout(function () { $(".xmark").remove(); }, 500);
+		    that.MinusPoints();
+		});
+		$("input[name='popquizchoice']").click(function () {
+		    var correct = $(this).attr("data-correct");
+		    if (correct == "true") {
+		        that.AddPoints();
+		        that.finishFunction();
+            }
+		    else {
+		        $(this).prop('checked', false);
+		        that.MinusPoints();
+		        that.incorrectFunction();
+            }
+		});
+
 	    // Intro Screen
 		$("#GoButton").click(function () {
 		    if ($('#username').val() != "")
@@ -84,51 +120,108 @@
 		    $("#points").text(that.score);
 		    $("#ui-info").show();
 		    $("#Modal-StartSpotLevel1").modal("hide");
-		});
-		$(".spot").click(function () {
-		    var id = $(this).attr("data-id");
-		    if (!$(this).hasClass("found"))
-		    {
-		        that.numbFound++;
-		        $(".spot[data-id='" + id + "']").each(function () {
-		            $(this).addClass("found");
-		            $(this).css("border", "5px solid white");
-		        });
-		        that.AddPoints();
-		    }
-		    if(that.numbFound == 3)
-		    {
-		        $("#Modal-EndSpotLevel1").modal("show");
-            }
+		    that.numbFound = 0;
+		    that.toBeFound = 3;
+		    that.finishFunction = function () { $("#Modal-EndSpotLevel1").modal("show"); };
 		});
 		$("#End-SpotLevel1").click(function () {
 		    $("#Modal-EndSpotLevel1").modal("hide");
 		    $(gameCarousel).carousel('next');
 		    that.startTime = Date.now();
+		    $("#Modal-StartQuizLevel1").modal("show");
 		});
 
 	    // Level 1 Quiz
-		$("input[name='popquiz1']").click(function () {
-		    var correct = $(this).attr("data-correct");
-		    if(correct == "true")
-		    {
-		        that.AddPoints();
-		        $("#Modal-EndQuizLevel1").modal("show");
-            }
-		    else
-		    {
-		        $(this).prop('checked', false);
-		        that.MinusPoints();
-		        $("#Modal-IncorrectQuizLevel1").modal("show");
-            }
+		$("#Start-QuizLevel1").click(function () {
+		    $("#Modal-StartQuizLevel1").modal("hide");
+		    that.incorrectFunction = function () { $("#Modal-IncorrectQuizLevel1").modal("show"); };
+		    that.finishFunction = function () { $("#Modal-EndQuizLevel1").modal("show"); };
 		});
 		$("#End-QuizLevel1").click(function () {
 		    $("#Modal-EndQuizLevel1").modal("hide");
 		    $(gameCarousel).carousel('next');
-		    that.startTime = Date.now();
+		    $("#Modal-StartBonusLevel1").modal("show");
 		});
 
-		
+	    // Level 1 Bonus
+		$("#Start-BonusLevel1").click(function () {
+		    $("#Modal-StartBonusLevel1").modal("hide");
+		    that.startTime = Date.now();
+		    that.numbFound = 0;
+		    that.toBeFound = 1;
+		    that.finishFunction = function () { $("#Modal-EndBonusLevel1").modal("show"); };
+		});
+		$("#End-BonusLevel1").click(function () {
+		    $("#Modal-EndBonusLevel1").modal("hide");
+		    $(gameCarousel).carousel('next');
+		});
+
+	    // Level 2 Intro Screen
+		$("#Level2IntroScreen").click(function () {
+		    $(gameCarousel).carousel('next');
+		    $("#Modal-StartSpotLevel2").modal("show");
+		});
+	    // Level 2 Spot
+		$("#Start-SpotLevel2").click(function () {
+		    that.startTime = Date.now();
+		    $("#points").text(that.score);
+		    $("#ui-info").show();
+		    $("#Modal-StartSpotLevel2").modal("hide");
+		    that.numbFound = 0;
+		    that.toBeFound = 5;
+		    that.finishFunction = function () { $("#Modal-EndSpotLevel2").modal("show"); };
+		});
+		$("#End-SpotLevel2").click(function () {
+		    $("#Modal-EndSpotLevel2").modal("hide");
+		    $(gameCarousel).carousel('next');
+		    that.startTime = Date.now();
+		    $("#Modal-StartBonusLevel2").modal("show");
+		});
+	    // Level 2 Bonus
+		$("#Start-BonusLevel2").click(function () {
+		    $("#Modal-StartBonusLevel2").modal("hide");
+		    that.startTime = Date.now();
+		    that.numbFound = 0;
+		    that.toBeFound = 1;
+		    that.finishFunction = function () { $("#Modal-EndBonusLevel2").modal("show"); };
+		});
+		$("#End-BonusLevel2").click(function () {
+		    $("#Modal-EndBonusLevel2").modal("hide");
+		    $(gameCarousel).carousel('next');
+		    $("#Modal-StartQuizLevel2").modal("show");
+		});
+	    // Level 2 Quiz
+		$("#Start-QuizLevel2").click(function () {
+		    $("#Modal-StartQuizLevel2").modal("hide");
+		    that.incorrectFunction = function () { $("#Modal-IncorrectQuizLevel2").modal("show"); };
+		    that.finishFunction = function () { $("#Modal-EndQuizLevel2").modal("show"); };
+		});
+		$("#End-QuizLevel2").click(function () {
+		    $("#Modal-EndQuizLevel2").modal("hide");
+		    $(gameCarousel).carousel('next');
+		});
+
+	    // Level 3 Intro Screen
+		$("#Level3IntroScreen").click(function () {
+		    $(gameCarousel).carousel('next');
+		    $("#Modal-StartSpotLevel3").modal("show");
+		});
+	    // Level 2 Spot
+		$("#Start-SpotLevel3").click(function () {
+		    that.startTime = Date.now();
+		    $("#points").text(that.score);
+		    $("#ui-info").show();
+		    $("#Modal-StartSpotLevel3").modal("hide");
+		    that.numbFound = 0;
+		    that.toBeFound = 5;
+		    that.finishFunction = function () { $("#Modal-EndSpotLevel3").modal("show"); };
+		});
+		$("#End-SpotLevel3").click(function () {
+		    $("#Modal-EndSpotLevel3").modal("hide");
+		    $(gameCarousel).carousel('next');
+		    that.startTime = Date.now();
+		   // $("#Modal-StartBonusLevel3").modal("show");
+		});
     },
 	
 	/**
